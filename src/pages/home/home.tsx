b8c102b63c7eabfa4a2d0b6094970f50273091de
Diego Brandjes <diego.brandjes@hva.nl>
2024-07-25 03:52:49 +0200

    reworked naming of multiple styles

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -9,13 +9,13 @@ function home() {
           <a
             href="/"
             aria-current="page"
-            className="link-block-4 w-inline-block w--current"
+            className="logo-link w-inline-block w--current"
           >
             <img
               src="https://cdn.prod.website-files.com/669fd653a78ebf02669a936a/66a0180d23b0eabef44432b2_Component%201.svg"
               loading="lazy"
               alt=""
-              className="image"
+              className="logo"
             />
           </a>
           <div className="w-layout-hflex navbar_btns">
@@ -29,18 +29,18 @@ function home() {
         </div>
       </section>
 
-      <section className="panel-1">
+      <section className="panel">
         <div className="imagefield">
           <img
             src="https://cdn.prod.website-files.com/669fd653a78ebf02669a936a/66a0179f2e77d8897c51aa5d_DSCF4565.jpg"
             loading="lazy"
             sizes="100vw"
             alt=""
-            className="image-3"
+            className="big-image"
           />
         </div>
         
-        <section className="infofield">
+        <section className="infofield-dark">
           <div className="div-block-overlap">
             <h1 className="heading">Matsumoto</h1>
             <p className="paragraph">
@@ -50,41 +50,41 @@ function home() {
               noodles. The city's proximity to the Japanese Alps makes it a
               gateway for outdoor enthusiasts and nature lovers.
             </p>
-            <a href="/" aria-current="page" className="link w--current">
+            <a href="/" aria-current="page" className="link-light w--current">
               See more of the Nakasendo chapter →{" "}
             </a>
           </div>
         </section>
       </section>
 
-      <section className="panel-2">
-        <div className="image-border"></div>
-        <section className="infofieldlight">
+      <section className="panel">
+        <div className="image-wide-border-1"></div>
+        <section className="infofield-light">
           <div className="div-block-no-overlap">
-            <h1 className="heading-copy">Sinai Desert </h1>
-            <p className="paragraph-copy">
+            <h1 className="heading">Sinai Desert </h1>
+            <p className="paragraph">
               A vast and rugged expanse in Egypt, is known for its dramatic
               landscapes, including towering mountains and vast sand dunes. This
               desert is a blend of stark beauty and profound spiritual heritage.
             </p>
-            <a href="/" aria-current="page" className="link-copy w--current">
+            <a href="/" aria-current="page" className="link-dark w--current">
               See more of the Egypt chapter →{" "}
             </a>
           </div>
         </section>
       </section>
 
-      <section className="panel-1">
+      <section className="panel">
         <div className="imagefield">
           <img
             src="https://cdn.prod.website-files.com/669fd653a78ebf02669a936a/66a05373c0c1ae6b5b1c6433_DSCF4919.jpg"
             loading="lazy"
             sizes="100vw"
             alt=""
-            className="image-3"
+            className="big-image"
           />
         </div>
-        <section className="infofield">
+        <section className="infofield-dark">
           <div className="div-block-overlap">
             <h1 className="heading">Ueno</h1>
             <p className="paragraph">
@@ -95,31 +95,33 @@ function home() {
               featuring attractions like Ueno Zoo, museums, and the lively
               Ameya-Yokocho market.
             </p>
-            <a href="/" aria-current="page" className="link w--current">
+            <a href="/" aria-current="page" className="link-light w--current">
               See more of the Tokyo chapter →{" "}
             </a>
           </div>
         </section>
       </section>
 
-      <section className="panel-2">
-        <div className="image-wide"></div>
-        <div className="imagefield-wborder"></div>
-        <section className="infofieldlight">
+      <section className="panel">
+        <div className="image-wide-border-2"></div>
+        <div className="image-wide-border-3"></div>
+
+        <section className="infofield-light">
           <div className="div-block-no-overlap">
-            <h1 className="heading-copy">Fukuoka </h1>
-            <p className="paragraph-copy">
+            <h1 className="heading">Fukuoka </h1>
+            <p className="paragraph">
               Fukuoka, a vibrant city on Japan's Kyushu island, is renowned for
               its lively yattai culture. These mobile food stalls line the
               streets, offering a variety of delicious local dishes such as
               ramen, tempura, and yakitori, creating a unique and charming
               dining experience.
             </p>
-            <a href="/" aria-current="page" className="link-copy w--current">
+            <a href="/" aria-current="page" className="link-dark w--current">
               See more of the Fukuoka chapter →{" "}
             </a>
           </div>
         </section>
+        
       </section>
     </body>
   );
