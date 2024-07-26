db388da66faf4a4509234cbe1a0f2bb384a9671d
Diego Brandjes <diego.brandjes@hva.nl>
2024-07-26 17:28:37 +0200

    renamed class

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -42,13 +42,13 @@ function contact() {
         
         <div className="spacer"></div>
         
-        <section className="infofield-small">
+        <section className="infofield-transparent">
           <div className="div-block-overlap">
-            <p className="paragraph-light">
+            <p className="paragraph-light less-wide-text">
             Based in the Netherlands with a passion for travel photography. 
             Exploring new places and capturing their atmosphere with a vintage tint. 
             </p>
-            <p className="paragraph-light">
+            <p className="paragraph-light less-wide-text">
             Follow my adventures and see the world through my eyes on this website or if you want a different experience 
             please check out my Instagram or other socials linked below. 
             </p>
