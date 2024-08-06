815c320619fc8bde8aa159c7e40df9818b37deb5
Diego Brandjes <diego.brandjes@hva.nl>
2024-08-06 02:03:42 +0200

    new menu style

--- src/compontents/navbar.tsx
+++ src/compontents/navbar.tsx
@@ -5,11 +5,11 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ dark }) => {
-    const [more, setMore] = useState(false);
     const [location, setLocation] = useState(false);
 
     return (
-        <section className="navbar">
+        <section >
+            <div className="navbar">
             <div className="w-layout-hflex flex-block">
                 <a href="/home" aria-current="page" className="">
                     <img src={dark ? "/logo-dark.svg" : "/logo.svg"} loading="lazy" alt="" className="logo" />
@@ -18,122 +18,79 @@ const Navbar: React.FC<NavbarProps> = ({ dark }) => {
                 {/* {/* BIGSCREEN NAVBAR */}
                 <div className="w-layout-hflex navbar_btns big-screen">
                     <div className="navbar_btns">
-                        <div className="block ">
-                            
-                            <div className={dark ? "dark-font dark-button" : "light-font button"} onClick={()=>{
-                                setLocation(!location)
-                                setMore(false)
-                                }}>
-                                <p className="other-font">
-                                    Locations
-                                </p>
-                            </div>
-
-                            {location && (
-                                    <div className="dropdown-content">
-                                        <a href="/country/japan" aria-current="page" className={dark ? "dark-font dark-button" : "light-font button"}>
-                                            <p className="other-font">
-                                            Japan
-                                            </p>            
-                                        </a>
-
-                                        <a href="/country/italy" aria-current="page" className={dark ? "dark-font dark-button" : "light-font button"}>
-                                            <p className="other-font">
-                                            Italy
-                                            </p>            
-                                        </a>
-
-                                        <a href="/country/egypt" aria-current="page" className={dark ? "dark-font dark-button" : "light-font button"}>
-                                            <p className="other-font">
-                                            Egypt
-                                            </p>            
-                                        </a>
-                                </div>
-                            )}
+                        <div className="block">
+                            <p className="other-font">
+                                <a href="#menu" className={dark ? "dark-font other-font" : "light-font other-font"}>Menu</a>
+                            </p>
                         </div>
                         
                         <div className="block">
-                            <a href="/gear" aria-current="page" className={dark ? "dark-font dark-button" : "light-font button"}>
-                                <p className="other-font">
-                                Gear 
-                                </p>            
-                            </a>
-                        </div>
-
-                        <div className="block">
-                            <a href="/contact" aria-current="page" className={dark ? "dark-font dark-button" : "light-font button"}>
-                                <p className="other-font">
-                                Contact 
-                                </p>            
-                            </a>
+                            <p className="other-font">
+                                <a href="/contact" className={dark ? "dark-font other-font dark-button" : "light-font other-font light-button"}>Contact</a>
+                            </p>            
                         </div>
 
                     </div>
                 </div>
 
-
-
-                {/* {/* SMALL NAVBAR */}
+                {/* SMALL NAVBAR */}
                 <div className="w-layout-hflex burger-container small-screen">
                     <div className="burger-container">
-                        <div className="block red">
-                            
-                            
-                            <div className={dark ? "hamburger-lines dark-font" : "hamburger-lines light-font light-button"} onClick={()=>{
-                                setLocation(!location)
-                                setMore(false)
+                        <a href="#menu" className="block ">
+                            <div className={dark ? "hamburger-lines dark-font" : "hamburger-lines light-font light-button"} onClick={() => {
+                                setLocation(!location);
                             }}>
-                                <span className="line line1"></span>
-                                <span className="line"></span>
-                                <span className="line line3"></span>
+                                <span className={dark ? "dark-line line1" : "line line1"}></span>
+                                <span className={dark ? "dark-line line2" : "line line2"}></span>
+                                <span className={dark ? "dark-line line3" : "line line3"}></span>
                             </div>
+                        </a>
 
-                            {location && (
-                                    <div className="dropdown-content">
-                                        <a href="/country/japan" aria-current="page" className={dark ? "dark-font dark-button" : "light-font button"}>
-                                            <p className="other-font">
-                                            Japan
-                                            </p>            
-                                        </a>
-
-                                        <a href="/country/italy" aria-current="page" className={dark ? "dark-font dark-button" : "light-font button"}>
-                                            <p className="other-font">
-                                            Italy
-                                            </p>            
-                                        </a>
-
-                                        <a href="/country/egypt" aria-current="page" className={dark ? "dark-font dark-button" : "light-font button"}>
-                                            <p className="other-font">
-                                            Egypt
-                                            </p>            
-                                        </a>
-
-                                        <a href="/country/egypt" aria-current="page" className={dark ? "dark-font dark-button" : "light-font button"}>
-                                            <p className="other-font">
-                                            Gear
-                                            </p>            
-                                        </a>
-
-                                        <a href="/country/egypt" aria-current="page" className={dark ? "dark-font dark-button" : "light-font button"}>
-                                            <p className="other-font">
-                                            Contact
-                                            </p>            
-                                        </a>
-                                </div>
-                            )}
-                        </div>
-                        
-                        
+                       
                     </div>
                 </div>
-
-
-
-
-                
-                    
             </div>
+            </div>
+
+                <div className="popover" id="menu">
+                    <div className = 'content'>
+                        <a href="#" className="close"></a>
+                        <div className = 'nav'>
+                            <ul className = 'nav_list'>
+                            
+                            <div className = 'nav_list_item'>                                
+                                <li><a href="/home">Home</a></li>
+                            </div>
+                            <div className = 'nav_list_item'>
+                                <li><a href="/country/japan">Japan</a></li>
+                            </div>
+                            <div className = 'nav_list_item'>
+                                <li><a href="/country/egypt">Egypt</a></li>
+                            </div>
+                            <div className = 'nav_list_item'>
+                                <li><a href="/country/italy">Italy</a></li>
+                            </div>
+                            <div className = 'nav_list_item'>
+                                <li><a href="/gear">Gear</a></li>
+                            </div>
+                            <div className="small-screen">
+                                <div className = 'nav_list_item'>
+                                    <li><a href="/contact">Contact</a></li>
+                                </div>
+                            </div>
+                            
+                            </ul>
+                        </div>
+                    </div>
+            </div>
+
+
         </section>
+
+
+
+
+
     );
 }
 
