b234af16992fddf32aafb99e9e609fa3d6170d8a
Diego Brandjes <diego.brandjes@hva.nl>
2024-08-07 01:44:33 +0200

    reworked links in gear page

--- src/pages/gear/gear.tsx
+++ src/pages/gear/gear.tsx
@@ -1,11 +1,6 @@
 import Navbar from "../../compontents/navbar";
 import "./gear.css";
 
-function handleClick() {
-    window.open('https://www.canonclassics.com/canon-nfd-35-70mm-f3.5-4.5/10-113/', '_blank'); // Opens the first URL in a new tab
-    window.open('https://fujifilm-x.com/en-us/products/lenses/xc15-45mmf35-56-ois-pz/', '_blank'); // Opens the second URL in a new tab
-}
-
 function gear() {
 
 return (
@@ -22,18 +17,13 @@ return (
                     <section className="infofield-transparent less-wide-text">
                         <div className="div-block-no-overlap">
                         <h2 className="light-font">Camera</h2>
-                        <p className=" light-font">
-                            The Fujifilm X-T30 II is a compact, versatile mirrorless camera that boasts 
+                        <a className=" light-font">
+                            The <a href="https://fujifilm-x.com/en-us/products/cameras/x-t30-ii/" className="link-light w--current">Fujifilm X-T30 II </a>
+                            is a compact, versatile mirrorless camera that boasts 
                             a 26.1MP APS-C X-Trans CMOS 4 sensor and X-Processor 4.
-                        </p>
-                        <p className="light-font">
                             It offers fast autofocus, 4K video recording, and impressive low-light 
-                            performance. 
-                        </p>
-                        <p className="light-font">
-                            I've been using this camera since 2023.
-                        </p>
-                        <a href="https://fujifilm-x.com/en-us/products/cameras/x-t30-ii/" className="link-light w--current">Click here to see the camera</a>
+                            performance. I've been using this camera since 2023.
+                        </a>
                         </div>
                     </section>
                     </div>
@@ -50,18 +40,13 @@ return (
                     <section className="infofield-transparent less-wide-text">
                         <div className="div-block-no-overlap">
                             <h2 className="light-font">Lenses</h2>
-                            <p className="light-font">
-                                My main lens is the versatile Fujifilm 15-45mm lens, 
+                            <a className="light-font">
+                                My main lens is the versatile <a href="https://fujifilm-x.com/en-us/products/lenses/xc15-45mmf35-56-ois-pz/" className="link-light w--current">Fujifilm 15-45mm</a> lens, 
                                 ideal for wide-angle shots and everyday use. 
-                            </p>
-                            <p className="light-font">
-                                My second lens is the Canon FD 35-70mm, 
-                                combined with an x-mount converter. 
-                            </p>
-                            <p className="light-font">
+                                My second lens is the <a href="https://www.canonclassics.com/canon-nfd-35-70mm-f3.5-4.5/10-113/" className="link-light w--current">Canon FD 35-70mm</a>, 
+                                combined with an x-mount converter.
                                 Both of these lenses cover a wide range of photographic needs. 
-                            </p>
-                            <a href="#" onClick={handleClick} className="link-light w--current">Click here to see the lens</a>
+                            </a>
                         </div>
                     </section>
                     </div>
