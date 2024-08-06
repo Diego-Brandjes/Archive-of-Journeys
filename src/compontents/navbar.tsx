30d2381a6127fb93b9a5b1c192df7562cc570677
Diego Brandjes <diego.brandjes@hva.nl>
2024-08-07 00:32:41 +0200

    reworked navbar and gear pages

--- src/compontents/navbar.tsx
+++ src/compontents/navbar.tsx
@@ -1,95 +1,98 @@
-import React, { useState } from "react";
+import React, { } from "react";
 
 interface NavbarProps {
     dark?: boolean;
 }
 
+const handleGoBack = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault(); // Prevent the default anchor behavior
+    window.history.back(); // Go back one page in the browser history
+};
+
 const Navbar: React.FC<NavbarProps> = ({ dark }) => {
-    const [location, setLocation] = useState(false);
-
     return (
-        <section >
+        <section>
             <div className="navbar">
-            <div className="w-layout-hflex flex-block">
-                <a href="/home" aria-current="page" className="">
-                    <img src={dark ? "/logo.svg" : "/logo-dark.svg"} loading="lazy" alt="" className="logo" />
-                </a>
+                <div className="w-layout-hflex flex-block">
+                    <a href="/home" aria-current="page" className="">
+                        <img 
+                            src={dark ? "/logo.svg" : "/logo-dark.svg"} 
+                            loading="lazy" 
+                            alt="Logo" 
+                            className="logo" 
+                        />
+                    </a>
 
-                {/* {/* BIGSCREEN NAVBAR */}
-                <div className="w-layout-hflex navbar_btns big-screen">
-                    <div className="navbar_btns">
-                        <div className="block">
-                            <p className="other-font">
-                                <a href="#menu" className={dark ? "light-font other-font" : "dark-font other-font"}>Menu</a>
-                            </p>
-                        </div>
-                        
-                        <div className="block">
-                            <p className="other-font">
-                                <a href="/contact" className={dark ? "light-font other-font" : "dark-font other-font"}>Contact</a>
-                            </p>            
-                        </div>
-
-                        {/*Toggle for darkmode  */}
-                        {/* <div className="block">
-                            <p className="other-font">
-                                <a href="#" className={dark ? "light-font other-font" : "dark-font other-font"}>☀</a>
-                            </p> 
-                        </div> */}
-
-                    </div>
-                </div>
-
-                {/* SMALL NAVBAR */}
-                <div className="w-layout-hflex burger-container small-screen">
-                    <div className="burger-container">
-                        <a href="#menu" className="block ">
-                            <div className={dark ? "hamburger-lines light-font" : "hamburger-lines dark-font"} onClick={() => {
-                                setLocation(!location);
-                            }}>
-                                <span className={dark ? "line line1" : "dark-line line1"}></span>
-                                <span className={dark ? "line line2" : "dark-line line2"}></span>
-                                <span className={dark ? "line line3" : "dark-line line3"}></span>
+                    {/* BIGSCREEN NAVBAR */}
+                    <div className="w-layout-hflex navbar_btns big-screen">
+                        <div className="navbar_btns">
+                            <div className="block">
+                                <p className="other-font">
+                                    <a href="#menu" className={dark ? "light-font other-font" : "dark-font other-font"}>Menu</a>
+                                </p>
                             </div>
-                        </a>   
+                            
+                            <div className="block">
+                                <p className="other-font">
+                                    <a href="/contact" className={dark ? "light-font other-font" : "dark-font other-font"}>Contact</a>
+                                </p>            
+                            </div>
+
+                            <div className="block">
+                                <p className="other-font">
+                                    <a href="#" onClick={handleGoBack} className={dark ? "light-font other-font" : "dark-font other-font"}>Return</a>
+                                </p> 
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* SMALL NAVBAR */}
+                    <div className="w-layout-hflex burger-container small-screen">
+                        <div className="burger-container">
+                            <a href="#menu" className="block">
+                                <div 
+                                    className={dark ? "hamburger-lines light-font" : "hamburger-lines dark-font"}>
+                                    <span className={dark ? "line line1" : "dark-line line1"}></span>
+                                    <span className={dark ? "line line2" : "dark-line line2"}></span>
+                                    <span className={dark ? "line line3" : "dark-line line3"}></span>
+                                </div>
+                            </a>   
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
 
-                <div className="popover" id="menu">
-                    <div className = 'content'>
-                        <a href="#" className="close"></a>
-                        <div className = 'nav'>
-                            <ul className = 'nav_list'>
-                            
-                            <div className = 'nav_list_item'>                                
+            <div className="popover" id="menu">
+                <div className="content">
+                    <a href="#" className="close"></a>
+                    <div className="nav">
+                        <ul className="nav_list">
+                            <div className="nav_list_item">
                                 <li><a href="/home">Home</a></li>
                             </div>
-                            <div className = 'nav_list_item'>
+                            <div className="nav_list_item">
                                 <li><a href="/country/japan">Japan</a></li>
                             </div>
-                            <div className = 'nav_list_item'>
+                            <div className="nav_list_item">
                                 <li><a href="/country/egypt">Egypt</a></li>
                             </div>
-                            <div className = 'nav_list_item'>
+                            <div className="nav_list_item">
                                 <li><a href="/country/italy">Italy</a></li>
                             </div>
-                            <div className = 'nav_list_item'>
+                            <div className="nav_list_item">
                                 <li><a href="/gear">Gear</a></li>
                             </div>
                             <div className="small-screen">
-                                <div className = 'nav_list_item'>
+                                <div className="nav_list_item">
                                     <li><a href="/contact">Contact</a></li>
                                 </div>
                             </div>
-                            
-                            </ul>
-                        </div>
+                        </ul>
                     </div>
+                </div>
             </div>
         </section>
     );
-}
+};
 
 export default Navbar;
--- src/pages/gear/gear.tsx
+++ src/pages/gear/gear.tsx
@@ -1,53 +1,73 @@
 import Navbar from "../../compontents/navbar";
 import "./gear.css";
 
+function handleClick() {
+    window.open('https://www.canonclassics.com/canon-nfd-35-70mm-f3.5-4.5/10-113/', '_blank'); // Opens the first URL in a new tab
+    window.open('https://fujifilm-x.com/en-us/products/lenses/xc15-45mmf35-56-ois-pz/', '_blank'); // Opens the second URL in a new tab
+}
+
 function gear() {
+
 return (
     <body className="body primary">
-      <Navbar dark={false} />
+      <Navbar dark={true} />
         <div className="spacer"></div>
 
-        <div className="w-layout-hflex flex-block-side">
-            <section className="infofield primary">
-                <div className="div-block-no-overlap">
-                    <h2 className="light-font">Camera</h2>
-                    <p className=" light-font">
-                        The Fujifilm X-T30 II is a compact, versatile mirrorless camera that boasts 
-                        a 26.1MP APS-C X-Trans CMOS 4 sensor and X-Processor 4.
-                    </p>
-                    <p className="light-font">
-                        It offers fast autofocus, 4K video recording, and impressive low-light 
-                        performance. 
-                    </p>
-                    <p className="light-font">
-                        I've been using this camera since 2023.
-                    </p>
-                </div>
-            </section>
-            <div className="image-block-border" style={{ backgroundImage: `url("/xt30ii.jpg")` }}></div>
-        </div>
-        
+        <a aria-current="page" className='link-light'>
+            <div className="w-layout-hflex flex-block-side">
+                <div className="image-wide-title">
+                    <img src="./xt30ii.jpg" alt="Image" />
 
-        <div className="w-layout-hflex flex-block-side">
-        <div className="image-block-border" style={{ backgroundImage: `url("/fuji-lens.jpg")` }}></div>
-        <section className="infofield primary">
-                <div className="div-block-no-overlap">
-                    <h2 className="light-font">Lenses</h2>
-                    <p className="light-font">
-                        My main lens is the versatile Fujifilm 15-45mm lens, 
-                        ideal for wide-angle shots and everyday use. 
-                    </p>
-                    <p className="light-font">
-                        My second lens is the Canon FD 35-70mm, 
-                        combined with an x-mount converter. 
-                    </p>
-                    <p className="light-font">
-                        Both of these lenses cover a wide range of photographic needs. 
-                    </p>
+                    <div className="overlay">
+                    <section className="infofield-transparent less-wide-text">
+                        <div className="div-block-no-overlap">
+                        <h2 className="light-font">Camera</h2>
+                        <p className=" light-font">
+                            The Fujifilm X-T30 II is a compact, versatile mirrorless camera that boasts 
+                            a 26.1MP APS-C X-Trans CMOS 4 sensor and X-Processor 4.
+                        </p>
+                        <p className="light-font">
+                            It offers fast autofocus, 4K video recording, and impressive low-light 
+                            performance. 
+                        </p>
+                        <p className="light-font">
+                            I've been using this camera since 2023.
+                        </p>
+                        <a href="https://fujifilm-x.com/en-us/products/cameras/x-t30-ii/" className="link-light w--current">Click here to see the camera</a>
+                        </div>
+                    </section>
+                    </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </a>
 
+        <a aria-current="page" className='link-light'>
+            <div className="w-layout-hflex flex-block-side">
+                <div className="image-wide-title">
+                    <img src="./fuji-lens.jpg" alt="Image" />
+
+                    <div className="overlay">
+                    <section className="infofield-transparent less-wide-text">
+                        <div className="div-block-no-overlap">
+                            <h2 className="light-font">Lenses</h2>
+                            <p className="light-font">
+                                My main lens is the versatile Fujifilm 15-45mm lens, 
+                                ideal for wide-angle shots and everyday use. 
+                            </p>
+                            <p className="light-font">
+                                My second lens is the Canon FD 35-70mm, 
+                                combined with an x-mount converter. 
+                            </p>
+                            <p className="light-font">
+                                Both of these lenses cover a wide range of photographic needs. 
+                            </p>
+                            <a href="#" onClick={handleClick} className="link-light w--current">Click here to see the lens</a>
+                        </div>
+                    </section>
+                    </div>
+                </div>
+            </div>
+        </a>
     </body>
 );
 }
